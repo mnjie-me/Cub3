4f7c7c21b01233e2cd7a2f3e655081c513b19883
Ana Ruiz Del Arbol Fuente <anruiz-d@c1r11s5.42madrid.com>
2026-01-12 16:42:38 +0100

    finish

--- srcs/parser/utils_scene.c
+++ srcs/parser/utils_scene.c
@@ -6,19 +6,21 @@
 /*   By: anruiz-d <anruiz-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 12:22:53 by mari-cruz         #+#    #+#             */
-/*   Updated: 2026/01/12 15:48:25 by anruiz-d         ###   ########.fr       */
+/*   Updated: 2026/01/12 16:40:15 by anruiz-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3.h"
 
-void	check_texture_path(char *path, t_data *data, char *cordinate)
+void	check_texture_path(char *path, t_data *data)
 {
 	int	fd;
+	size_t	len;
 
 	if (!path)
 		ft_end(data, "Error : Missing texture path");
-	if (ft_strncmp(path, cordinate, ft_strlen(cordinate)))
+	len = ft_strlen(path);
+	if (len < 4 || ft_strnstr(path, ".xpm", len) != (path + len - 4))
 		ft_end(data, "Error : Missing texture path");
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
@@ -28,8 +30,8 @@ void	check_texture_path(char *path, t_data *data, char *cordinate)
 
 void	check_all_texture_paths(t_data *data)
 {
-	check_texture_path(data->tex.no, data, "NO srcs/textures/NO.xpm");
-	check_texture_path(data->tex.so, data, "EA srcs/textures/EA.xpm");
-	check_texture_path(data->tex.we, data, "WE srcs/textures/WE.xpm");
-	check_texture_path(data->tex.ea, data, "SO srcs/textures/SO.xpm");
+	check_texture_path(data->tex.no, data);
+	check_texture_path(data->tex.so, data);
+	check_texture_path(data->tex.we, data);
+	check_texture_path(data->tex.ea, data);
 }
